2448de97d997e29009ea500dbc27b0b6a8e9f2c2
wensputra2026 <putraens@gmail.com>
2026-09-05 17:48:42 +0800

    fix(keuskupan): use generic Catholic diocese crest as fallback instead of Kupang logo

--- app/Models/Keuskupan.php
+++ app/Models/Keuskupan.php
@@ -67,7 +67,7 @@ class Keuskupan extends Model
      */
     public function getLogoUrlAttribute(): string
     {
-        $default = asset('images/logo-keuskupan.png');
+        $default = asset('images/default-keuskupan.svg');
 
         if (!empty($this->attributes['logo'])) {
             $relative = ltrim((string) $this->attributes['logo'], '/');
